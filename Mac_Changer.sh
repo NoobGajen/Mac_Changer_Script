51aa1e15ac56f31f63116b8d618243d7a1284b0a
NoobGajen <sudogajendra@gmail.com>
2022-08-17 07:56:39 +0545

    Mac changer with automation without creating file

---
+++ Mac_Changer.sh
@@ -0,0 +1,14 @@
+for i in $(iwconfig 2>&1 | grep -oP "^\w+" | grep -v lo)
+do
+	
+### Mac changing message accordingly interface
+echo -e "\n#######Changing_Mac_for_$i#######"
+
+########Code to Vailed Mac changer######
+ouimac=$(macchanger -l | shuf -n 1 | awk '{print$3}')
+uaamac=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
+ifconfig "$i" down
+macchanger -m  "$ouimac:$uaamac" "$i"
+ifconfig "$i" up
+done
+
