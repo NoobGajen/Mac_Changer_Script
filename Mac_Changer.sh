0b0ff9fba595ebc1cb14025303ce299466c7cd4a
NoobGajen <sudogajendra@gmail.com>
2022-08-19 18:41:29 +0545

    Mac changer script

--- Mac_Changer.sh
+++ Mac_Changer.sh
@@ -2,7 +2,7 @@ for i in $(iwconfig 2>&1 | grep -oP "^\w+" | grep -v lo)
 do
 	
 ### Mac changing message accordingly interface
-echo -e "\n#######Changing_Mac_for_$i#######"
+echo -e "\n#######_Changing_Mac_for_( $i )_#######"
 
 ########Code to Vailed Mac changer######
 ouimac=$(macchanger -l | shuf -n 1 | awk '{print$3}')
